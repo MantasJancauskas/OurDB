c329bb9ac18a6176a31b403077160b11152ec128
MantasJancauskas <mantas.jancauskas1@gmail.com>
2022-09-27 12:55:06 +0300

    Added update successful msg and if duplicate error message

--- insertdata.php
+++ insertdata.php
@@ -2,19 +2,20 @@
 require 'connect.php';
 
 print_r($_POST);
-try{
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$jobId = $_POST['jobId'];
+try {
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $email = $_POST['email'];
+    $jobId = $_POST['jobId'];
 
     $sql = "INSERT INTO Employees (firstname, lastname, email, jobId) VALUES('$firstname', '$lastname', '$email', '$jobId')";
-    if ( $conn->query($sql) ) {
-        session_start();
+    if ($conn->query($sql)) {
+
         $_SESSION['msg'] = 'Update Successfully.';
-    header("location: index.php");
-    exit();
-}
-}catch(EXCEPTION) {
-    header("location: index.php");
+        header("location: index.php");
+        exit();
+    }
+} catch (EXCEPTION) {
+    header("location: insert.php");
+    $_SESSION['msg'] = 'Create new worker failed, please use unique email.';
 }
